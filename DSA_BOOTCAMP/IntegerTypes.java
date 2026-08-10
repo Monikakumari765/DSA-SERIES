1bd5698f0d83b31a9617396875e1d1a7777fdbf3
Monika kumari <monikakumari4687@gmail.com>
2026-08-10 17:41:47 +0530

    bootcamp

---
+++ DSA_BOOTCAMP/IntegerTypes.java
@@ -0,0 +1,23 @@
+public class IntegerTypes {
+
+    public static void main(String[] args) {
+
+        byte b = 100;
+
+        short s = 25000;
+
+        int i = 1000000;
+
+        long l = 9876543210L;
+
+        System.out.println(b);
+
+        System.out.println(s);
+
+        System.out.println(i);
+
+        System.out.println(l);
+
+    }
+
+}
