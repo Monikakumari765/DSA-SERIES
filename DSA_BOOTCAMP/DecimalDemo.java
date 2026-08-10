af84c6c3499978b6f63ff0de907cc9785dacb813
Monika kumari <monikakumari4687@gmail.com>
2026-08-10 17:42:50 +0530

    bootcamp

---
+++ DSA_BOOTCAMP/DecimalDemo.java
@@ -0,0 +1,15 @@
+public class DecimalDemo {
+
+    public static void main(String[] args) {
+
+        float temperature = 37.5f;
+
+        double salary = 55000.756789;
+
+        System.out.println(temperature);
+
+        System.out.println(salary);
+
+    }
+
+}
